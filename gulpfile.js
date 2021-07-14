1c2a2b5c435baff95f8a4c81effdab98fa76afca
yazid <yazid.abdrasid@gmail.com>
2021-07-14 18:11:32 +0800

    Test commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var cleanCss = require('gulp-clean-css');
 var rename = require('gulp-rename');
 
